3d1d068677142e5a4f4cc7d66695725efa53e80e
ravindra <ravindra.patil2@tcs.com>
2019-09-18 09:32:14 +0530

    final change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,22 +25,4 @@ describe('AppComponent', () => {
     let service: LoaderService = TestBed.get(LoaderService);
     expect(service).toBeTruthy();
   });
-
-  // it('should create the app', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   fixture.detectChanges();
-  //   expect(app).toBeTruthy();
-  // }));
-  //   it(`should have as title 'hello-world'`, async(() => {
-  //     const fixture = TestBed.createComponent(AppComponent);
-  //     const app = fixture.debugElement.componentInstance;
-  //     expect(app.title).toEqual('hello-world');
-  //   }));
-  //   it('should render title in a h1 tag', async(() => {
-  //     const fixture = TestBed.createComponent(AppComponent);
-  //     fixture.detectChanges();
-  //     const compiled = fixture.debugElement.nativeElement;
-  //     expect(compiled.querySelector('h1').textContent).toContain('Welcome to hello-world!');
-  // }));
 });
--- src/app/services/loader/loader.service.spec.ts
+++ src/app/services/loader/loader.service.spec.ts
@@ -14,14 +14,4 @@ describe('LoaderService', () => {
         const service: LoaderService = TestBed.get(LoaderService);
         expect(service).toBeTruthy();
     });
-    // it('should be created LoaderService', async(inject([LoaderService], (LoaderService) => {        
-    //     spyOn(LoaderService, 'display').and.callFake((args) => console.log(args));
-    //     expect(LoaderService.status).toEqual(false);
-    // })));
-
-    // it('Should retrieves display flag by calling display()', async(inject([LoaderService], (LoaderService) => {
-    //     return LoaderService.display().toPromise().then((result) => {
-    //         expect(result.length).toBeGreaterThan(0);
-    //     });
-    // })));
 });
